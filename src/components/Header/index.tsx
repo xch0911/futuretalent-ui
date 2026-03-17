bfc9e8a99fb31affe304425640bc50a3ee2c676b
xch0911 <futuretalent@arkclaw>
2026-03-17 22:02:58 +0800

    优化移动端导航：添加汉堡菜单抽屉

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { Layout, Input, Button, Avatar, Dropdown, MenuProps } from 'antd'
-import { SearchOutlined, UserOutlined, PlusOutlined } from '@ant-design/icons'
+import { Layout, Input, Button, Avatar, Dropdown, MenuProps, Drawer } from 'antd'
+import { SearchOutlined, UserOutlined, PlusOutlined, MenuOutlined } from '@ant-design/icons'
 import { User } from '@/types'
 import styles from './index.module.css'
 
@@ -11,6 +11,7 @@ const Header: React.FC = () => {
   const location = useLocation()
   const navigate = useNavigate()
   const [searchText, setSearchText] = useState('')
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const isLoggedIn = false // TODO: 从状态管理获取登录状态
   const userInfo: User | null = null // TODO: 从状态管理获取用户信息
@@ -83,36 +84,125 @@ const Header: React.FC = () => {
           </div>
 
           <div className={styles.actions}>
-            {isLoggedIn ? (
-              <>
-                <Button 
-                  type="primary" 
-                  icon={<PlusOutlined />}
-                  onClick={() => navigate('/idea/create')}
-                >
-                  发布想法
-                </Button>
-                <Dropdown menu={{ items }} placement="bottomRight">
-                  <Avatar 
-                    size="small"
-                    src={user ? user.avatar : undefined}
-                    icon={<UserOutlined />}
-                    className={styles.avatar}
-                    style={{ cursor: 'pointer' }}
-                  />
-                </Dropdown>
-              </>
-            ) : (
-              <>
-                <Button onClick={() => navigate('/login')}>登录</Button>
-                <Button type="primary" onClick={() => navigate('/register')} style={{ marginLeft: 8 }}>
-                  注册
-                </Button>
-              </>
-            )}
+            {/* 移动端汉堡菜单按钮 */}
+            <Button
+              type="text"
+              icon={<MenuOutlined />}
+              className={styles.menuButton}
+              onClick={() => setMenuOpen(true)}
+            />
+
+            {/* 桌面端正常显示 */}
+            <div className={styles.desktopActions}>
+              {isLoggedIn ? (
+                <>
+                  <Button 
+                    type="primary" 
+                    icon={<PlusOutlined />}
+                    onClick={() => navigate('/idea/create')}
+                  >
+                    发布想法
+                  </Button>
+                  <Dropdown menu={{ items }} placement="bottomRight">
+                    <Avatar 
+                      size="small"
+                      src={user ? user.avatar : undefined}
+                      icon={<UserOutlined />}
+                      className={styles.avatar}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </Dropdown>
+                </>
+              ) : (
+                <>
+                  <Button onClick={() => navigate('/login')}>登录</Button>
+                  <Button type="primary" onClick={() => navigate('/register')} style={{ marginLeft: 8 }}>
+                    注册
+                  </Button>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
+
+      {/* 移动端抽屉菜单 */}
+      <Drawer
+        title="菜单"
+        placement="right"
+        onClose={() => setMenuOpen(false)}
+        open={menuOpen}
+        width={240}
+      >
+        <div className={styles.mobileMenu}>
+          <Link 
+            to="/" 
+            className={styles.mobileNavLink}
+            onClick={() => setMenuOpen(false)}
+          >
+            首页
+          </Link>
+          <Link 
+            to="/ideas" 
+            className={styles.mobileNavLink}
+            onClick={() => setMenuOpen(false)}
+          >
+            想法广场
+          </Link>
+          <div className={styles.mobileSearch}>
+            <Input
+              placeholder="搜索..."
+              prefix={<SearchOutlined />}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              onPressEnter={() => {
+                handleSearch()
+                setMenuOpen(false)
+              }}
+            />
+          </div>
+          {isLoggedIn ? (
+            <>
+              <Button 
+                type="primary" 
+                block
+                icon={<PlusOutlined />}
+                style={{ marginTop: 16 }}
+                onClick={() => {
+                  navigate('/idea/create')
+                  setMenuOpen(false)
+                }}
+              >
+                发布想法
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button 
+                block 
+                style={{ marginTop: 16 }}
+                onClick={() => {
+                  navigate('/login')
+                  setMenuOpen(false)
+                }}
+              >
+                登录
+              </Button>
+              <Button 
+                type="primary" 
+                block
+                style={{ marginTop: 8 }}
+                onClick={() => {
+                  navigate('/register')
+                  setMenuOpen(false)
+                }}
+              >
+                注册
+              </Button>
+            </>
+          )}
+        </div>
+      </Drawer>
     </AntHeader>
   )
 }
